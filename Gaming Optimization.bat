bcd4ec7c436381d66ddbb49f3616d45ac1447b16
crustySenpai <83429736+crustySenpai@users.noreply.github.com>
2021-08-13 21:06:51 +0200

    Update Gaming Optimization.bat

--- Gaming Optimization.bat
+++ Gaming Optimization.bat
@@ -1,5 +1,5 @@
 @echo off
-title Gaming Optimization Beta by crustySenpai
+title Gaming Optimization by crustySenpai
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cls
 :start
